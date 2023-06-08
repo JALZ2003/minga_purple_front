2fa7d39220438e385ecd462be87ff4b76fbbb50f
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-06-07 23:30:56 -0500

    Se Finalizo el sprint 1, con la sidebar desplegable y el carrusel funcional (#4)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,22 @@
-// import userImage from '../assets/estefaUser.png';
-// import clouse from '../assets/clouse.svg';
+import { useState } from 'react';
 import logoWeb from '../assets/logoWeb.svg';
 import logoMobile from '../assets/LogoMobile.svg';
 import menu from '../assets/Menu.svg';
+import Sidebar from './Sidebar';
 
 function Navbar() {
+    const options = [{ title: "Home" }, { title: "Comics" }, { title: "My Comics" }, { title: "Favorites" }, { title: "Logout" }];
+    const [show, setShow] = useState(false);
     return (
         <>
             <nav className='flex justify-between w-full absolute'>
-                <button className='w-[55px] h-[55px] ml-5 mt-5 lg:ml-[5%]'>
+                <button className='w-[55px] h-[55px] ml-5 mt-5 lg:ml-[5%]' onClick={() => setShow(!show)}>
                     <img src={menu} alt="Menu" />
                 </button>
                 <img className='w-[40px] h-[48px] inline-block lg:hidden mr-5 mt-5 lg:mr-[5%]' src={logoMobile} alt="Minga" />
                 <img className='w-[140px] h-[50px] hidden lg:inline-block mr-5 mt-5 lg:mr-[5%]' src={logoWeb} alt="Minga" />
             </nav>
-
-            {/* <nav id='nav' className='invisible fixed text-white list-none flex flex-col items-center bg-gradient-to-t from-[#F97316] to-[#FF5722] h-screen w-full lg:w-[430px]'>
-                <li className='flex justify-between w-full'>
-                    <div className='flex m-5'>
-                        <img className='m-2 rounded-[50px] w-[46px] h-[46px]' src={userImage} alt="" />
-                        <div className='m-2'>
-                            <h1 className='font-poppins font-semibold text-[16px] leading-[24px] -tracking-[.01em]'> Estefania Marin </h1>
-                            <p className='font-poppins font-medium text-[14px leading-[12px] -tracking-[.01em]'> estefaniamarin@gmail.com </p>
-                        </div>
-                    </div>
-                    <button className='m-5'><img src={clouse} alt="" /></button>
-                </li>
-                <li className='cursor-pointer w-[382px] h-[48px] rounded-lg p-3 gap-3 text-center hover:bg-white hover:text-[#FF5722]'>
-                    <a href="#" className='font-poppins font-semibold text-[15px] leading-[24px] -tracking-[.01em]'> Home </a>
-                </li>
-                <li className='cursor-pointer w-[382px] h-[48px] rounded-lg p-3 gap-3 text-center hover:bg-white hover:text-[#FF5722]'>
-                    <a href="#" className='font-poppins font-semibold text-[15px] leading-[24px] -tracking-[.01em]'> Comics </a>
-                </li>
-                <li className='cursor-pointer w-[382px] h-[48px] rounded-lg p-3 gap-3 text-center hover:bg-white hover:text-[#FF5722]'>
-                    <a href="#" className='font-poppins font-semibold text-[15px] leading-[24px] -tracking-[.01em]'> My Comics </a>
-                </li>
-                <li className='cursor-pointer w-[382px] h-[48px] rounded-lg p-3 gap-3 text-center hover:bg-white hover:text-[#FF5722]'>
-                    <a href="#" className='font-poppins font-semibold text-[15px] leading-[24px] -tracking-[.01em]'> Favorites </a>
-                </li>
-                <li className='cursor-pointer w-[382px] h-[48px] rounded-lg p-3 gap-3 text-center hover:bg-white hover:text-[#FF5722]'>
-                    <a href="#" className='font-poppins font-semibold text-[15px] leading-[24px] -tracking-[.01em]'> Logout </a>
-                </li>
-            </nav> */}
+            { show && <Sidebar options={options} show={show} setShow={setShow} />}
         </>
     )
 }
